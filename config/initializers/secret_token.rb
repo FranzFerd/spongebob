2998e5b0a7c4e2f37f78c6b0c9afdd6a9a1e25dc
FranzFerd <sponsorfy@gmail.com>
2013-06-12 23:15:13 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spongebob::Application.config.secret_token = '6792c2e276289673c83225be112119c0af2d78960da80e14113f7840c63562f6d7eb9e092cfbf5c347d479cb77face286bc4c85e728b1a4734ecbe22926f7c52'
